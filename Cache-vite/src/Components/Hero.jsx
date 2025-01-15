54d4f1fca12fbaa35d08572833e2d8bb3fc0f2ba
Michael Harris <158084149+Mikemike8@users.noreply.github.com>
2025-01-15 14:14:36 -0600

    push

--- Cache-vite/src/Components/Hero.jsx
+++ Cache-vite/src/Components/Hero.jsx
@@ -66,20 +66,22 @@ const Hero = () => {
             </nav>
           </section>
         </div>
-        <div className="absolute bottom-6 w-full flex flex-col sm:flex-row justify-center space-x-4 px-4">
+        <div className="absolute bottom-6 w-full flex flex-row justify-center space-x-4 px-4">
+
   <a href="/menu">
     <button className="bg-red-600 text-white font-medium py-2 px-6 rounded shadow-md border-2 border-transparent hover:bg-red-700 hover:shadow-red-600 hover:border-red-600 transition duration-300">
       View Menu
     </button>
   </a>
 
-  <a
-    href="https://www.opentable.com/restref/client/?rid=1279489&restref=1279489&partysize=2&datetime=2024-09-05T19%3A00&lang=en-US&r3uid=Md80nnnPF&ot_source=Restaurant%20website&color=1&corrid=ef28e07b-8f0c-47f3-a126-d91d3c7a78dc"
- >
-    <button className="bg-red-600 text-white font-medium py-2 px-6 rounded shadow-md border-2 border-transparent hover:bg-red-700 hover:shadow-red-600 hover:border-red-600 transition duration-300">Book A Table</button>
-    
+  
+  <a href="https://www.opentable.com/restref/client/?rid=1279489&restref=1279489&partysize=2&datetime=2024-09-05T19%3A00&lang=en-US&r3uid=Md80nnnPF&ot_source=Restaurant%20website&color=1&corrid=ef28e07b-8f0c-47f3-a126-d91d3c7a78dc">
+    <button className="bg-red-600 text-white font-medium py-2 px-6 rounded shadow-md border-2 border-transparent hover:bg-red-700 hover:shadow-red-600 hover:border-red-600 transition duration-300">
+      Book A Table
+    </button>
   </a>
 </div>
+
 
       </div>
     </>
