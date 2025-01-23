4999ac5effa820f446021f47f42d523b62da50b6
Michael Harris <158084149+Mikemike8@users.noreply.github.com>
2025-01-22 18:16:45 -0600

    wqe

--- Cache-vite/tailwind.config.js
+++ Cache-vite/tailwind.config.js
@@ -1,9 +1,9 @@
-// Use ESM syntax if your project is set to "module"
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  darkMode: "class",
+  darkMode: "class", // Enable dark mode using class
   theme: {
     extend: {
       keyframes: {
